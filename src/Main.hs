e538b8c1d20abef5c9c97ba7c4379b99931fb37b
silky <noonsilk+-noonsilk@gmail.com>
2022-06-15 22:18:10 +0100

    header stuff not working.

--- src/Main.hs
+++ src/Main.hs
@@ -11,12 +11,13 @@ import Data.Text                        (Text)
 import Network.Wai                      (Request, requestHeaders)
 import Servant                          (Handler, err401, err403, throwError, errBody,
                                         type (:>), (:<|>)(..), Get, AuthProtect, Proxy(Proxy),
-                                        Context((:.), EmptyContext), Server, serveWithContext)
+                                        Context((:.), EmptyContext), Server, serveWithContext,
+                                        Header, Headers(Headers), addHeader)
 import Network.Wai.Handler.Warp         (run)
 import Servant.HTML.Blaze               (HTML)
 import Servant.Server.Experimental.Auth (AuthServerData, AuthHandler , mkAuthHandler)
 import Text.Hamlet                      (Html, shamlet)
-import Web.Cookie                       (parseCookies)
+import Web.Cookie                       (parseCookies, SetCookie(..), sameSiteStrict, defaultSetCookie)
 
 newtype Account = Account { unAccount :: Text }
 
@@ -29,14 +30,47 @@ lookupAccount key
       Nothing   -> throwError (err403 { errBody = "Invalid Cookie" })
       Just user -> return user
 
-authHandler :: AuthHandler Request Account
+-- type AuthContext r = AuthHandler Request (SessionFor r)
+-- authHandler :: AuthContext 'Anyone
+-- authHandler :: AuthHandler Request SessionFor
+--
+type AuthContext = AuthHandler Request SessionFor
+
+authHandler :: AuthContext
 authHandler = mkAuthHandler handler
   where
     maybeToEither e = maybe (Left e) Right
     throw401 msg = throwError $ err401 { errBody = msg }
-    handler req = either throw401 lookupAccount $ do
-      cookie <- maybeToEither "Missing cookie header" $ lookup "cookie" $ requestHeaders req
-      maybeToEither "Missing token in cookie" $ lookup "servant-auth-cookie" $ parseCookies cookie
+    handler :: Request -> Handler SessionFor
+    handler req = do
+      let h = requestHeaders req
+          c = lookup "cookie" h
+      case c of
+        Nothing -> throw401 "Missing 'Cookie' header."
+        Just c' -> do
+          let p = parseCookies c'
+              ac = lookup "servant-auth-cookie" p
+          case ac of
+            Nothing  -> throw401 "Missing 'servant-auth-cookie' in cookie."
+            Just ac' -> do
+              acc <- lookupAccount ac'
+              pure $ (addHeader mkSessionCookie acc)
+
+mkSessionCookie :: SetCookie
+mkSessionCookie =
+  defaultSetCookie
+    { setCookieName     = "b3-secret-cookie"
+    , setCookieValue    = "cookie-data"
+    , setCookieMaxAge   = Just oneWeek
+    , setCookiePath     = Just "/"
+    , setCookieSameSite = Just sameSiteStrict
+    , setCookieHttpOnly = True
+    , setCookieSecure   = False
+    }
+    where
+      oneWeek = 3600 * 24 * 7
+
+type SessionFor = Headers '[Header "Set-Cookie" SetCookie] Account
 
 type AuthGenApi
   =    "private" :> AuthProtect "cookie-auth" :> Get '[HTML] Html
@@ -45,14 +79,14 @@ type AuthGenApi
 genAuthApi :: Proxy AuthGenApi
 genAuthApi = Proxy
 
-type instance AuthServerData (AuthProtect "cookie-auth") = Account
+type instance AuthServerData (AuthProtect "cookie-auth") = SessionFor
 
-genAuthServerContext :: Context '[AuthHandler Request Account]
+genAuthServerContext :: Context '[AuthContext]
 genAuthServerContext = authHandler :. EmptyContext
 
 genAuthServer :: Server AuthGenApi
 genAuthServer =
-  let privateDataFunc (Account name) = return $ [shamlet|this is secret: #{name}|]
+  let privateDataFunc (Headers (Account name) _) = return $ [shamlet|this is secret: #{name}|]
       publicData = return [shamlet|public data.|]
    in privateDataFunc :<|> publicData
 
